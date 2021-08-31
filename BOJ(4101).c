eddd79527c987909a3eb7910918719daae470a1e
jseungtaek <82442293+jseungtaek@users.noreply.github.com>
2021-08-31 22:50:45 +0900

    [BOJ] 4101:크냐?

---
+++ BOJ(4101).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//baekjoon 4101
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	while (a != 0 && b != 0)
+	{
+		if (a > b) printf("Yes\n");
+		else printf("No\n");
+		scanf("%d%d", &a, &b);
+	}
+	return 0;
+}

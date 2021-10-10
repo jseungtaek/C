ad9f293d5bc1162e2fc20ec5242e9b73e62d0e49
jseungtaek <82442293+jseungtaek@users.noreply.github.com>
2021-10-10 15:05:30 +0900

    C_rush00

---
+++ rush03.c
@@ -0,0 +1,74 @@
+void	ft_putchar(char c);
+
+void	ft_print_first_or_last_line(int x)
+{
+    int		x_length;
+    
+    x_length = 1;
+    ft_putchar('A');
+    while (x_length < x - 1)
+    {
+        ft_putchar('B');
+        x_length++;
+    }
+    if (x != 1)
+        ft_putchar('C');
+    ft_putchar('\\n');
+}
+void	ft_print_mid(int x, int y)
+{
+    int x_length;
+    int y_length;
+
+    y_length = 1;
+    while (y_length < y - 1)
+    {
+	ft_putchar('B');
+	x_length = 1;
+	while (x_length < x - 1)
+	{
+		ft_putchar(' ');
+		x_length++;
+	}
+	if (x != 1)
+		ft_putchar('B');
+	ft_putchar('\\n');
+	y_length++;
+  }
+}
+
+/*void	ft_print_last_line(int x, int y)
+{
+    int		x_length;
+
+    x_length = 1;
+    ft_putchar('C');
+    while (x_length < x - 1)
+    {
+        ft_putchar('-');
+        x_length++;
+    }
+    if (x != 1)
+        ft_putchar('A');
+    ft_putchar('\n')
+}*/
+
+void    rush(int x, int y)
+
+{
+    if (x > 0 && y > 0)
+    {
+        ft_print_first_or_last_line(x);
+        ft_print_else_line(x,y);
+        if( y > 1)
+            ft_print_first_or_last_line(x);
+    }
+    else if(x == 0 && y > 0)
+    {
+        while (y>1)
+        {
+            ft_putchar('\n');
+            y--;
+        }
+    }
+}
---
+++ rush04.c
@@ -0,0 +1,73 @@
+void	ft_putchar(char c);
+
+void	ft_print_first_line(int x)
+{
+    int		x_length;
+    
+    x_length = 1;
+    ft_putchar('A');
+    while (x_length < x - 1)
+    {
+        ft_putchar('B');
+        x_length++;
+    }
+    if (x != 1)
+        ft_putchar('C');
+    ft_putchar('\\n');
+}
+void	ft_print_mid_line(int x, int y)
+{
+    int x_length;
+    int y_length;
+
+    y_length = 1;
+    while (y_length < y - 1)
+    {
+	ft_putchar('B');
+	x_length = 1;
+	while (x_length < x - 1)
+	{
+		ft_putchar(' ');
+		x_length++;
+	}
+	if (x != 1)
+		ft_putchar('B');
+	ft_putchar('\\n');
+	y_length++;
+  }
+}
+
+void	ft_print_last_line(int x, int y)
+{
+    int		x_length;
+
+    x_length = 1;
+    ft_putchar('C');
+    while (x_length < x - 1)
+    {
+        ft_putchar('B');
+        x_length++;
+    }
+    if (x != 1)
+        ft_putchar('A');
+    ft_putchar('\n');
+}
+
+void    rush(int x, int y)
+
+{
+    if (x > 0 && y > 0)
+    {
+        ft_print_first_line(x);
+        ft_print_mid_line(x,y);
+        ft_print_last_line(x,y);
+    }
+    else if(x == 0 && y > 0)
+    {
+        while (y > 0)
+        {
+            ft_putchar('\n');
+            y--;
+        }
+    }
+}
